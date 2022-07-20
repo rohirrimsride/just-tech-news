948f244379e81d10c1b516e10d746b84b6578374
Strider <dave.barnes23@gmail.com>
2022-07-20 18:01:57 -0500

    added api routes for the post model

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+
 const userRoutes = require('../api/user-routes');
+const postRoutes = require('../api/post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
