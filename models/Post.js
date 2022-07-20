8886b729035b2c35aaf67e06f98c0374b5026836
Strider <dave.barnes23@gmail.com>
2022-07-20 16:48:09 -0500

    added the Post model

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const { Model, DataType, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create our Post model
+class Post extends Model {}
+// Create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
