e4ea6668fd0df2aeaefc0040cdcbebd442644f0e
Strider <dave.barnes23@gmail.com>
2022-08-06 14:41:56 -0500

    removed seeds and package-lock from gitignore file

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -147,17 +147,17 @@ router.delete('/:id', (req, res) => {
             id: req.params.id
         }
     })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
